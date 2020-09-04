16a4fbc292abe46101a386ba49c8f633ccafff3d
lParanhos <leandro.paranhos@dextra-sw.com>
2020-09-04 08:48:11 -0300

    create books route

--- main.go
+++ main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-func handlersConfig() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Bem vindo !")
-	})
+func welcomeRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bem vindo !")
+}
 
+func bookList(w http.ResponseWriter, r *http.Request) {
+	encode := json.NewEncoder(w)
+	encode.Encode(Books)
+}
+
+func handlersConfig() {
+	http.HandleFunc("/", welcomeRoute)
+	http.HandleFunc("/books", bookList)
+}
+
+type Book struct {
+	ID     int
+	Titulo string
+	Autor  string
+}
+
+var Books []Book = []Book{
+	Book{
+		ID:     1,
+		Titulo: "Eric Clapton",
+		Autor:  "Eric",
+	},
+	Book{
+		ID:     2,
+		Titulo: "Cazuza",
+		Autor:  "Viriato Correia",
+	},
+	Book{
+		ID:     3,
+		Titulo: "Dom Casmurro",
+		Autor:  "Machado de Assis",
+	},
 }
 
 func serverConfig() {
